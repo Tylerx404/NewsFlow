e146cddcd53a7e7061b3bcd48a7a6338566a2c86
datjpro <todat2207@gmail.com>
2026-03-02 15:42:14 +0700

    fix(worker): update dotenv config to use fileURLToPath for path resolution

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,13 +1,16 @@
 import { config } from "dotenv";
+import { fileURLToPath } from "node:url";
 
-config();
 config({
-  path: new URL("../../server/.env", import.meta.url).pathname,
+  path: fileURLToPath(new URL("../../server/.env", import.meta.url)),
   override: false,
 });
+
 
 const { env } = await import("@NewsFlow/env/server");
 const { startJobRunner } = await import("@NewsFlow/queue");
 
 console.log(`Worker is starting in ${env.NODE_ENV} mode`);
 startJobRunner();
+
+
